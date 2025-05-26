50bb39f671b18ea5d1115426c5b698cdd8cc7a2c
Geronimo <guastavinogeronimo1@gmail.com>
2025-05-26 15:39:16 -0300

    Correccion de los bordes blancos welcomefondo1

--- saludtotal/resources/views/welcome.blade.php
+++ saludtotal/resources/views/welcome.blade.php
@@ -71,7 +71,7 @@
             </aside>
             <!--/Sidebar-->
             <!--Main-->
-            <main class="bg-white-300 flex-1 p-3 overflow-hidden">
+            <main class="bg-white-300 flex-1 overflow-hidden">
                 <!--Shortcuts-->
                 <div class="flex flex-row justify-between items-center pb-4">
                     <div class="flex flex-1">
@@ -104,139 +104,140 @@
                 <!--/Shortcuts-->
                 <!--Content-->
                  <!--img src="{asset('img/WelcomeFondo1.png')}}" alt="salud total logo" class="w-full h-1/2"-->
-                 <section class="relative z-0 bg-cover bg-center py-20" style="background-image: url('{{ asset('img/WelcomeFondo1.jpg') }}')" >
-  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-    <h1 class="text-4xl font-bold text-black text-center mb-12 drop-shadow-lg">
-      Bienvenido a Salud Total
-    </h1>
+              <section class="relative z-0 bg-cover bg-center py-20" style="background-image: url('{{ asset('img/WelcomeFondo1.jpg') }}')" >
+                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                  <h1 class="text-4xl font-bold text-black text-center mb-12 drop-shadow-lg">
+                    Bienvenido a Salud Total
+                  </h1>
 
-     <div class="bg-white shadow-lg rounded-lg overflow-hidden mb-8">
-          <img
-    class="w-full h-48 object-cover object-center"
-    src="{{ asset('img/panel1.jpg') }}"
-    alt="Panel 1"
-  />
-        <div class="p-6">
-          <h3 class="text-xl font-semibold mb-2">Chequeos Médicos</h3>
-          <p class="text-gray-800">Realizarse un análisis de  sangre de forma  regular es  fundamental  para cuidar la salud. Este examen permite detectar de manera temprana posibles enfermedades, como la diabetes, el colesterol alto, problemas hepáticos, anemia o infecciones. Además, ayuda a monitorear el estado general del cuerpo y la efectividad de ciertos tratamientos si ya se está bajo cuidado médico.</p>
-        </div>
-      </div>
+                  <div class="bg-white shadow-lg rounded-lg overflow-hidden mb-8">
+                        <img
+                  class="w-full h-48 object-cover object-center"
+                  src="{{ asset('img/panel1.jpg') }}"
+                  alt="Panel 1"
+                />
+                      <div class="p-6">
+                        <h3 class="text-xl font-semibold mb-2">Chequeos Médicos</h3>
+                        <p class="text-gray-800">Realizarse un análisis de  sangre de forma  regular es  fundamental  para cuidar la salud. Este examen permite detectar de manera temprana posibles enfermedades, como la diabetes, el colesterol alto, problemas hepáticos, anemia o infecciones. Además, ayuda a monitorear el estado general del cuerpo y la efectividad de ciertos tratamientos si ya se está bajo cuidado médico.</p>
+                      </div>
+                    </div>
 
-      <div class="bg-white shadow-lg rounded-lg overflow-hidden mb-8">
-                  <img
-    class="w-full h-48 object-cover object-center"
-    src="{{ asset('img/panel2.png') }}"
-    alt="Panel 1"
-  />
-        <div class="p-6">
-          <h3 class="text-xl font-semibold mb-2">La importancia de tomarse la presion</h3>
-          <p class="text-gray-700">Tomarse la presión  arterial con un doctor permite evaluar el funcionamiento del corazón y el sistema circulatorio. Es un control simple, rápido y sin dolor, pero muy útil para detectar problemas como la hipertensión, una condición que muchas veces no presenta síntomas pero que puede causar serios daños si no se trata a tiempo.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-<section class="bg-white py-10">
-  <h1 class="text-2xl font-semibold text-center mb-8">Pedí tu turno</h1>
+                    <div class="bg-white shadow-lg rounded-lg overflow-hidden mb-8">
+                                <img
+                  class="w-full h-48 object-cover object-center"
+                  src="{{ asset('img/panel2.png') }}"
+                  alt="Panel 1"
+                />
+                      <div class="p-6">
+                        <h3 class="text-xl font-semibold mb-2">La importancia de tomarse la presion</h3>
+                        <p class="text-gray-700">Tomarse la presión  arterial con un doctor permite evaluar el funcionamiento del corazón y el sistema circulatorio. Es un control simple, rápido y sin dolor, pero muy útil para detectar problemas como la hipertensión, una condición que muchas veces no presenta síntomas pero que puede causar serios daños si no se trata a tiempo.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              <section class="bg-white py-10">
+                <h1 class="text-2xl font-semibold text-center mb-8">Pedí tu turno</h1>
 
-  <div class="flex justify-center items-center flex-wrap gap-x-12 gap-y-8">
+                <div class="flex justify-center items-center flex-wrap gap-x-12 gap-y-8">
 
-    <a href="/turnos/cardiologia" class="flex flex-col items-center hover:opacity-80 transition">
-      <img src="img/doctor.svg" alt="Cardiología" class="w-32 h-32 mb-2 rounded-xl p-3">
-      <span class="text-[#0C1C3C] font-semibold">Cardiología</span>
-    </a>
+                  <a href="/turnos/cardiologia" class="flex flex-col items-center hover:opacity-80 transition">
+                    <img src="img/doctor.svg" alt="Cardiología" class="w-32 h-32 mb-2 rounded-xl p-3">
+                    <span class="text-[#0C1C3C] font-semibold">Cardiología</span>
+                  </a>
 
-    <a href="/turnos/ginecologia" class="flex flex-col items-center hover:opacity-80 transition">
-      <img src="img/doctor.svg" alt="Ginecología" class="w-32 h-32 mb-2 rounded-xl p-3">
-      <span class="text-[#0C1C3C] font-semibold">Ginecología</span>
-    </a>
+                  <a href="/turnos/ginecologia" class="flex flex-col items-center hover:opacity-80 transition">
+                    <img src="img/doctor.svg" alt="Ginecología" class="w-32 h-32 mb-2 rounded-xl p-3">
+                    <span class="text-[#0C1C3C] font-semibold">Ginecología</span>
+                  </a>
 
-    <a href="/turnos/pediatria" class="flex flex-col items-center hover:opacity-80 transition">
-      <img src="img/doctor.svg" alt="Pediatría" class="w-32 h-32 mb-2 rounded-xl p-3">
-      <span class="text-[#0C1C3C] font-semibold">Pediatría</span>
-    </a>
+                  <a href="/turnos/pediatria" class="flex flex-col items-center hover:opacity-80 transition">
+                    <img src="img/doctor.svg" alt="Pediatría" class="w-32 h-32 mb-2 rounded-xl p-3">
+                    <span class="text-[#0C1C3C] font-semibold">Pediatría</span>
+                  </a>
 
-    <a href="/turnos/clinica-general" class="flex flex-col items-center hover:opacity-80 transition">
-      <img src="img/doctor.svg" alt="Clínica General" class="w-32 h-32 mb-2 rounded-xl p-3">
-      <span class="text-[#0C1C3C] font-semibold">Clínica General</span>
-    </a>
+                  <a href="/turnos/clinica-general" class="flex flex-col items-center hover:opacity-80 transition">
+                    <img src="img/doctor.svg" alt="Clínica General" class="w-32 h-32 mb-2 rounded-xl p-3">
+                    <span class="text-[#0C1C3C] font-semibold">Clínica General</span>
+                  </a>
 
-  </div>
-</section>
-<section class="relative z-0 bg-cover bg-center py-20" style="background-image: url('{{ asset('img/WelcomeFondo2.jpg') }}')" >
-    <h1 class="text-4xl font-bold text-black text-center mb-12 drop-shadow-lg">
-        NUESTROS SERVICIOS CLINICOS
-    </h1>
-<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 px-8 max-w-6xl mx-auto">
+                </div>
+              </section>
+              <section class="relative z-0 bg-cover bg-center py-20" style="background-image: url('{{ asset('img/WelcomeFondo2.jpg') }}')" >
+                  <h1 class="text-4xl font-bold text-black text-center mb-12 drop-shadow-lg">
+                      NUESTROS SERVICIOS CLINICOS
+                  </h1>
+              <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 px-8 max-w-6xl mx-auto">
 
-  <div class="rounded-xl overflow-hidden shadow-md mb-6">
-  <img src="img/Cardiologia.jpg" alt="Cardiologia" class="w-full h-48 object-cover" />
-  <div class="p-4 bg-white">
-    <h3 class="text-lg font-bold">Cardiología</h3>
-      <p class="text-sm text-gray-700">
-        Ofrecemos un diagnóstico y tratamiento integral de las enfermedades cardiovasculares, asegurando una atención continua y de calidad.
-      </p>
-    </div>
-  </div>
-  <div class="rounded-xl overflow-hidden shadow-md mb-6">
-  <img src="img/Pediatria.jpg" alt="Pediatria" class="w-full h-48 object-cover" />
-  <div class="p-4 bg-white">
-    <h3 class="text-lg font-bold">Pediatria</h3>
-      <p class="text-sm text-gray-700">
-        Cuidamos el crecimiento y desarrollo saludable de los más pequeños, especializada desde la infancia hasta la adolescencia.
-      </p>
-    </div>
-  </div>
+                <div class="rounded-xl overflow-hidden shadow-md mb-6">
+                <img src="img/Cardiologia.jpg" alt="Cardiologia" class="w-full h-48 object-cover" />
+                <div class="p-4 bg-white">
+                  <h3 class="text-lg font-bold">Cardiología</h3>
+                    <p class="text-sm text-gray-700">
+                      Ofrecemos un diagnóstico y tratamiento integral de las enfermedades cardiovasculares, asegurando una atención continua y de calidad.
+                    </p>
+                  </div>
+                </div>
+                <div class="rounded-xl overflow-hidden shadow-md mb-6">
+                <img src="img/Pediatria.jpg" alt="Pediatria" class="w-full h-48 object-cover" />
+                <div class="p-4 bg-white">
+                  <h3 class="text-lg font-bold">Pediatria</h3>
+                    <p class="text-sm text-gray-700">
+                      Cuidamos el crecimiento y desarrollo saludable de los más pequeños, especializada desde la infancia hasta la adolescencia.
+                    </p>
+                  </div>
+                </div>
 
-  <div class="rounded-xl overflow-hidden shadow-md mb-6">
-  <img src="img/Ginecologia.jpg" alt="Ginecologia" class="w-full h-48 object-cover" />
-  <div class="p-4 bg-white">
-    <h3 class="text-lg font-bold">Ginecología</h3>
-      <p class="text-sm text-gray-700">
-        Brindamos servicios completos de salud femenina, desde chequeos rutinarios hasta tratamientos especializados.
-      </p>
-    </div>
-  </div>
+                <div class="rounded-xl overflow-hidden shadow-md mb-6">
+                <img src="img/Ginecologia.jpg" alt="Ginecologia" class="w-full h-48 object-cover" />
+                <div class="p-4 bg-white">
+                  <h3 class="text-lg font-bold">Ginecología</h3>
+                    <p class="text-sm text-gray-700">
+                      Brindamos servicios completos de salud femenina, desde chequeos rutinarios hasta tratamientos especializados.
+                    </p>
+                  </div>
+                </div>
 
-  <div class="rounded-xl overflow-hidden shadow-md mb-6">
-  <img src="img/clinica-general.jpg" alt="Clinica General" class="w-full h-48 object-cover" />
-  <div class="p-4 bg-white">
-    <h3 class="text-lg font-bold">Clinica General</h3>
-      <p class="text-sm text-gray-700">
-        Proporcionamos atención médica general con un enfoque en la prevención y la salud integral.
-      </p>
-    </div>
-  </div>
-</section>
-<section class="relative z-0 bg-cover bg-center py-20" style="background-image: url('{{ asset('img/Consulta.jpg') }}')" >
-  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="rounded-xl overflow-hidden shadow-md mb-6">
+                <img src="img/clinica-general.jpg" alt="Clinica General" class="w-full h-48 object-cover" />
+                <div class="p-4 bg-white">
+                  <h3 class="text-lg font-bold">Clinica General</h3>
+                    <p class="text-sm text-gray-700">
+                      Proporcionamos atención médica general con un enfoque en la prevención y la salud integral.
+                    </p>
+                  </div>
+                </div>
+              </section>
+              <section class="relative z-0 bg-cover bg-center py-20" style="background-image: url('{{ asset('img/Consulta.jpg') }}')" >
+                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
-      <div class="bg-white shadow-lg rounded-lg overflow-hidden mb-8">
-         <h1 class="text-4xl font-bold text-black text-center mb-12 drop-shadow-lg">
-      HACER UNA CONSULTA ESPECIAL
-    </h1>
-        <div class="p-6">
-          <h3 class="text-xl font-semibold mb-2">Nombre</h3>
-          <input type="text" class="border border-gray-300 rounded-lg p-2 w-full mb-4" placeholder="Ingrese su nombre">
-          
-          <h3 class="text-xl font-semibold mb-2">Email</h3>
-          <input type="email" class="border border-gray-300 rounded-lg p-2 w-full mb-4" placeholder="Ingrese su email">
-          
-          <h3 class="text-xl font-semibold mb-2">Teléfono</h3>
-          <input type="text" class="border border-gray-300 rounded-lg p-2 w-full mb-4" placeholder="Ingrese su teléfono">
-          
-          <h3 class="text-xl font-semibold mb-2">Mensaje</h3>
-          <textarea class="border border-gray-300 rounded-lg p-2 w-full mb-4" rows="4" placeholder="Ingrese su mensaje"></textarea>
-          
-          <button class="bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-600">
-            Enviar
-          </button>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
+                    <div class="bg-white shadow-lg rounded-lg overflow-hidden mb-8">
+                      <h1 class="text-4xl font-bold text-black text-center mb-12 drop-shadow-lg">
+                    HACER UNA CONSULTA ESPECIAL
+                  </h1>
+                      <div class="p-6">
+                        <h3 class="text-xl font-semibold mb-2">Nombre</h3>
+                        <input type="text" class="border border-gray-300 rounded-lg p-2 w-full mb-4" placeholder="Ingrese su nombre">
+                        
+                        <h3 class="text-xl font-semibold mb-2">Email</h3>
+                        <input type="email" class="border border-gray-300 rounded-lg p-2 w-full mb-4" placeholder="Ingrese su email">
+                        
+                        <h3 class="text-xl font-semibold mb-2">Teléfono</h3>
+                        <input type="text" class="border border-gray-300 rounded-lg p-2 w-full mb-4" placeholder="Ingrese su teléfono">
+                        
+                        <h3 class="text-xl font-semibold mb-2">Mensaje</h3>
+                        <textarea class="border border-gray-300 rounded-lg p-2 w-full mb-4" rows="4" placeholder="Ingrese su mensaje"></textarea>
+                        
+                        <button class="bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-600">
+                          Enviar
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
                 <!--/Content-->
-
+              </section>
             </main>
 
         </div>
