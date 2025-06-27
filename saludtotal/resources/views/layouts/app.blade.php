faac688c1384e1152be172e2c311a2ac7255a667
Juan Manuel Fernandez <juanma08072003@gmail.com>
2025-06-26 23:53:22 -0300

    tailwin en cdn

--- saludtotal/resources/views/layouts/app.blade.php
+++ saludtotal/resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <!-- Css -->
     <link rel="stylesheet" href={{asset("css/styles.css")}}>
     <link rel="stylesheet" href={{asset("css/all.css")}}>
+    <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,600i,700,700i" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
     @viteReactRefresh
